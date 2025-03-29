a9eeb84ad66301222aea861ae291f26065b2cc08
SymuNadeem <msymunnadeem@gmail.com>
2025-03-29 15:09:05 +0600

    modal user added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
 const menuBtn = document.getElementById("menu-btn");
-    const closeBtn = document.getElementById("close-btn");
-    const sidebarMenu = document.getElementById("sidebar-menu");
-    const overlay = document.getElementById("overlay");
-    const body = document.body;
+const closeBtn = document.getElementById("close-btn");
+const sidebarMenu = document.getElementById("sidebar-menu");
 
-    function openMenu() {
-      sidebarMenu.style.width = "80%";
-      overlay.classList.remove("hidden");
-      body.classList.add("no-scroll");
-    }
+const userBtn = document.getElementById("user-btn");
+const closeUserBtn = document.getElementById("close-user-btn");
+const userModal = document.getElementById("user-modal");
 
-    function closeMenu() {
-      sidebarMenu.style.width = "0";
-      overlay.classList.add("hidden");
-      body.classList.remove("no-scroll");
-    }
+const overlay = document.getElementById("overlay");
+const body = document.body;
 
-    menuBtn.addEventListener("click", openMenu);
-    closeBtn.addEventListener("click", closeMenu);
-    overlay.addEventListener("click", closeMenu);
+function openMenu() {
+  sidebarMenu.style.width = "80%";
+  overlay.classList.remove("hidden");
+  body.classList.add("no-scroll");
+}
+
+function closeMenu() {
+  sidebarMenu.style.width = "0";
+  overlay.classList.add("hidden");
+  body.classList.remove("no-scroll");
+}
+
+function openUserModal() {
+  userModal.style.width = "510px";
+  overlay.classList.remove("hidden");
+  body.classList.add("no-scroll");
+}
+
+function closeUserModal() {
+  userModal.style.width = "0";
+  overlay.classList.add("hidden");
+  body.classList.remove("no-scroll");
+}
+
+menuBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+
+userBtn.addEventListener("click", openUserModal);
+closeUserBtn.addEventListener("click", closeUserModal);
+
+overlay.addEventListener("click", () => {
+  closeMenu();
+  closeUserModal();
+});
