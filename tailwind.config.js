e94c77ec3c59b82112cd22c36e88c681e8807e63
SymuNadeem <msymunnadeem@gmail.com>
2025-03-29 11:29:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#05285E",
+        // secondary: "#FACC15", 
+      },
+    },
+  },
+  plugins: [],
+}
