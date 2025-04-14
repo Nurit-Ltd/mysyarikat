32daea5622d5fc84f936e0a707bcdcd942650cd8
SymuNadeem <msymunnadeem@gmail.com>
2025-04-14 21:41:43 +0600

    dropdown icon rotate

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,23 +2,21 @@ document.querySelectorAll(".dropdown-toggle").forEach((toggleBtn) => {
   toggleBtn.addEventListener("click", () => {
     const dropdownMenu = toggleBtn.nextElementSibling;
     const arrowIcon = toggleBtn.querySelector(".arrow-icon");
-
-    const isOpen =
-      dropdownMenu.style.height && dropdownMenu.style.height !== "0px";
+    const isOpen = dropdownMenu.style.height && dropdownMenu.style.height !== "0px";
 
     if (isOpen) {
       dropdownMenu.style.height = dropdownMenu.scrollHeight + "px";
       requestAnimationFrame(() => {
         dropdownMenu.style.height = "0px";
       });
-      arrowIcon.classList.remove("rotate-90");
+      arrowIcon.style.transform = "rotate(0deg)";
     } else {
       dropdownMenu.style.height = dropdownMenu.scrollHeight + "px";
       dropdownMenu.addEventListener("transitionend", function handler() {
         dropdownMenu.style.height = "auto";
         dropdownMenu.removeEventListener("transitionend", handler);
       });
-      arrowIcon.classList.add("rotate-90");
+      arrowIcon.style.transform = "rotate(-90deg)";
     }
   });
 });
